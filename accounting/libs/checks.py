051f87244e6524d9a392b90e15cfabb25f9ce8d1
Max Gong <mg@io21.me>
2020-05-14 11:23:18 +0400

    resolve older version reverse import, and sorteddict

--- accounting/libs/checks.py
+++ accounting/libs/checks.py
@@ -1,5 +1,4 @@
 from django.core.validators import EMPTY_VALUES
-from django.utils.datastructures import SortedDict
 
 
 class PrimaryKeyRelatedField(object):
@@ -114,7 +113,7 @@ class CheckingModelMixin(object):
         """
         Returns the set of fields on which we perform checkings
         """
-        ret = SortedDict()
+        ret = {}
         for f in self._meta.fields:
             # avoid special_exclude fields
             if f.attname in special_exclude:
@@ -135,7 +134,7 @@ class CheckingModelMixin(object):
 
         # If 'fields' is specified, use those fields, in that order.
         if self.opts.fields:
-            new = SortedDict()
+            new = {}
             for key in self.opts.fields:
                 new[key] = ret[key]
             ret = new
